537280bec96f10d6d10d741fffc296574efa6795
iyiola olusola ebenezer <sholaiyiola1966@gmail.com>
2024-07-02 09:47:15 +1000

    initial commit

---
+++ SpeedDetector.js
@@ -0,0 +1,18 @@
+let marks = 79;
+
+if( marks <=79 && marks >= 100){
+    console.log('You Got A');
+} else if (mark <=60 && marks>= 79){
+    console.log('YOU GOT B')
+} else if ( mark < 49 && marks >=59){
+    console.log('YOU GOT C')
+}else if(mark <=49 && marks >=50){
+    console.log('YOU GOT D')
+} else if(mark <=40 && mark>=0 ){
+    console.log('YOU GOT E')
+
+
+}
+else {
+    console.log('out of range');
+}
